ecc1b212ae1970c76bb11e04bfc3c314c3b48306
3292049030kj-web <3292049030kj@gmail.com>
2025-11-28 06:58:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,26 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-from openai import OpenAI
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+import openai
 
-BOT_TOKEN = 8204695273:AAFsodphzWnC9TbEhMN3_-A9mWIoE12ukKY("BOT_TOKEN")
-OPENAI_API_KEY =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("OPENAI_API_KEY")
+BOT_TOKEN = "8204695273:AAFsodphzWnC9TbEhMN3_-A9mWIoE12ukKY"
+OPENAI_API_KEY = "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"
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+openai.api_key = OPENAI_API_KEY
 
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def reply(update, context):
     user_text = update.message.text
 
-    response = client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": user_text}
-        ],
+        messages=[{"role": "user", "content": user_text}],
     )
 
-    bot_reply = response.choices[0].message.content
-    await update.message.reply_text(bot_reply)
+    bot_reply = response["choices"][0]["message"]["content"]
+    update.message.reply_text(bot_reply)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply))
+updater = Updater(BOT_TOKEN, use_context=True)
+handler = MessageHandler(Filters.text & (~Filters.command), reply)
+updater.dispatcher.add_handler(handler)
 
-app.run_polling()
+updater.start_polling()
+updater.idle()
